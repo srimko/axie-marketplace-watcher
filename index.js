acee1c88ce843e57fff9ecef3d9ea9fa4cefb835
Alexandre Cédrick <alexandre.cedrick@gmail.com>
2021-07-20 11:16:44 +0200

    Add a new button to get latest eggs available
    Add 3 buttons to get latest axies and eggs available from the store and some refactoring

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
-function getAxies() {
+async function getAxies(type) {
   const myHeaders = new Headers();
 
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Origin", "https://marketplace.axieinfinity.com");
   myHeaders.append("Referer", "https://marketplace.axieinfinity.com");
 
+  if (type === undefined) {
+    type = 'axies'
+  }
+
+  const bodyRequest = {
+    axies: { "operationName": "GetAxieLatest", "variables": { "from": 0, "size": 10, "sort": "Latest", "auctionType": "Sale" }, "query": "query GetAxieLatest($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {    total    results {      ...AxieRowData      __typename    }    __typename  }}fragment AxieRowData on Axie {  id  image  class  name  genes  owner  class  stage  title  breedCount  level  parts {    ...AxiePart    __typename  }  stats {    ...AxieStats    __typename  }  auction {    ...AxieAuction    __typename  }  __typename}fragment AxiePart on AxiePart {  id  name  class  type  specialGenes  stage  abilities {    ...AxieCardAbility    __typename  }  __typename}fragment AxieCardAbility on AxieCardAbility {  id  name  attack  defense  energy  description  backgroundUrl  effectIconUrl  __typename}fragment AxieStats on AxieStats {  hp  speed  skill  morale  __typename}fragment AxieAuction on Auction {  startingPrice  endingPrice  startingTimestamp  endingTimestamp  duration  timeLeft  currentPrice  currentPriceUSD  suggestedPrice  seller  listingIndex  state  __typename}" },
+    eggs: { "operationName": "GetAxieBriefList", "variables": { "from": 0, "size": 10, "sort": "Latest", "auctionType": "Sale", "owner": null, "criteria": { "region": null, "parts": null, "bodyShapes": null, "classes": null, "stages": [1], "numMystic": null, "pureness": null, "title": null, "breedable": null, "breedCount": null, "hp": [], "skill": [], "speed": [], "morale": [] } }, "query": "query GetAxieBriefList($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {    total    results {      ...AxieBrief      __typename    }    __typename  }}fragment AxieBrief on Axie {  id  name  stage  class  breedCount  image  title  battleInfo {    banned    __typename  }  auction {    currentPrice    currentPriceUSD    __typename  }  parts {    id    name    class    type    specialGenes    __typename  }  __typename}" }
+  }
+
+  console.log(bodyRequest[type])
+
   const myInit = {
     method: 'POST',
     headers: myHeaders,
-    body: JSON.stringify({ "operationName": "GetAxieLatest", "variables": { "from": 0, "size": 10, "sort": "Latest", "auctionType": "Sale" }, "query": "query GetAxieLatest($auctionType: AuctionType, $criteria: AxieSearchCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {  axies(auctionType: $auctionType, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {    total    results {      ...AxieRowData      __typename    }    __typename  }}fragment AxieRowData on Axie {  id  image  class  name  genes  owner  class  stage  title  breedCount  level  parts {    ...AxiePart    __typename  }  stats {    ...AxieStats    __typename  }  auction {    ...AxieAuction    __typename  }  __typename}fragment AxiePart on AxiePart {  id  name  class  type  specialGenes  stage  abilities {    ...AxieCardAbility    __typename  }  __typename}fragment AxieCardAbility on AxieCardAbility {  id  name  attack  defense  energy  description  backgroundUrl  effectIconUrl  __typename}fragment AxieStats on AxieStats {  hp  speed  skill  morale  __typename}fragment AxieAuction on Auction {  startingPrice  endingPrice  startingTimestamp  endingTimestamp  duration  timeLeft  currentPrice  currentPriceUSD  suggestedPrice  seller  listingIndex  state  __typename}" })
+    body: JSON.stringify(bodyRequest[type])
   };
 
-  fetch('https://axieinfinity.com/graphql-server-v2/graphql', myInit)
-    .then((req, res) => {
-      req.json().then(r => {
-        const axies = r.data.axies.results
+  const response = await fetch('https://axieinfinity.com/graphql-server-v2/graphql', myInit)
 
-        let localAxies = []
-        axies.forEach(axie => {
-          if (/([0-9]+)/.exec(axie.image) !== null) {
-            localAxies.push(axie)
-          }
-        })
-        displayAxies(localAxies)
-        localStorage.setItem('axies', JSON.stringify(localAxies))
-        localStorage.setItem('lastUpdate', Date.now())
-      })
-    })
-    .catch(error => {
-      console.log(error)
-    })
+  if (!response.ok) {
+    throw new Error(`Erreur HTTP ! statut : ${response}`);
+  }
+
+  const json = await response.json()
+  const axies = json.data.axies.results
+  let localAxies = []
+  axies.forEach(axie => {
+    if (/([0-9]+)/.exec(axie.image) !== null) {
+      localAxies.push(axie)
+    } else if (/(egg)/.exec(axie.image) !== null) {
+      localAxies.push(axie)
+    }
+  })
+  const refresh = document.querySelector('.refresh')
+  displayAxies(localAxies, type)
+  localStorage.setItem('axies', JSON.stringify(localAxies))
+  localStorage.setItem('lastUpdate', Date.now())
 }
 
-function refreshAxies() {
-  getAxies()
+function refreshAxies(type) {
+  console.log(type)
+  getAxies(type).catch(e => {
+    refresh.classList.remove('processing')
+    console.log(e.message);
+  });
 }
 
 function displayLastUpdate(timestamp) {
@@ -57,46 +73,87 @@ function displayLastUpdate(timestamp) {
   lastUpdateElement.textContent = formattedTime
 }
 
-function displayAxies(axies) {
+function displayAxies(axies, type = 'axies') {
   let axiesHTML = ''
   axies.forEach(axie => {
-    console.log(axie)
-    if (/([0-9]+)/.exec(axie.image) !== null) {
-      axiesHTML += `<div class="axies ${axie.class.toLowerCase()}">
+    if (type === 'axies') {
+      if (/([0-9]+)/.exec(axie.image) !== null) {
+        axiesHTML += `<div class="axies ${axie.class.toLowerCase()}">
+              <div class="header">
+                <div class="axie-image" style="background-image: url(${axie.image});">
+                  <img src="${axie.image}" />
+                </div>
+                <div>
+                  <span class="${axie.class.toLowerCase()}">
+                    #${axie.id}
+                  </span>
+                  <p>Breed count: ${axie.breedCount}</p>
+                </div>
+              </div>
+              <div class="body">
+                <a href="https://marketplace.axieinfinity.com/axie/${/([0-9]+)/.exec(axie.image)[0]}" target="_blank">${axie.name}</a>
+              </div>
+              <div>
+                <p>Ξ 0.${axie.auction.currentPrice.slice(0, 4)} USD</p>
+                <p>$${axie.auction.currentPriceUSD} USD</p>
+              </div>
+            </div>`
+      }
+    } else {
+      if (/(egg)/.exec(axie.image) !== null) {
+        axiesHTML += `<div class="axies ">
           <div class="header">
             <div class="axie-image" style="background-image: url(${axie.image});">
               <img src="${axie.image}" />
             </div>
             <div>
-              <span class="${axie.class.toLowerCase()}">
+              <span class="">
                 #${axie.id}
               </span>
               <p>Breed count: ${axie.breedCount}</p>
             </div>
           </div>
           <div class="body">
-            <a href="https://marketplace.axieinfinity.com/axie/${/([0-9]+)/.exec(axie.image)[0]}" target="_blank">${axie.name}</a>
+            <a href="https://marketplace.axieinfinity.com/axie/${axie.id}" target="_blank">${axie.name}</a>
           </div>
           <div>
             <p>Ξ 0.${axie.auction.currentPrice.slice(0, 4)} USD</p>
             <p>$${axie.auction.currentPriceUSD} USD</p>
           </div>
         </div>`
+      }
     }
   })
   document.querySelector('#text').innerHTML = axiesHTML
+
+  const refresh = document.querySelector('.refresh')
+  refresh.classList.remove('processing')
 
   displayLastUpdate()
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   console.log('Go fetch axie\'s API')
+  let type = 'axies'
+
 
   const refresh = document.querySelector('.refresh')
+  const buttons = document.querySelectorAll('[data-type]')
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', e => {
+      type = e.target.dataset.type
+      refresh.classList.add('processing')
+      getAxies(type).catch(e => {
+        refresh.classList.remove('processing')
+        console.log(e.message);
+      });
+    })
+  })
 
   refresh.addEventListener('click', (event) => {
     console.log('Refresh')
-    refreshAxies()
+    refreshAxies(type)
   })
 
   const localAxies = JSON.parse(localStorage.getItem('axies'))
@@ -105,6 +162,10 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log(localAxies)
     displayAxies(localAxies)
   } else {
-    getAxies()
+    getAxies(type).catch(e => {
+      const refresh = document.querySelector('.refresh')
+      refresh.classList.remove('processing')
+      console.log(e.message);
+    });
   }
 })
